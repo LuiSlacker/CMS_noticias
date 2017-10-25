f0b1fa0172ae26d79675ef252877af959de6d21f
Petulant Slacker <l.goohsen@gmail.com>
2017-10-25 14:08:28 -0500

    cleanup

--- config/config.js
+++ config/config.js
@@ -3,13 +3,13 @@
 module.exports = {
   host: (process.env.NODE_ENV === 'production') ? 'math.citeplag.org' : `localhost:${process.env.PORT || '4001'}`,
   slack: {
-    webhook: 'https://hooks.slack.com/services/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',
+    webhook: 'https://hooks.slack.com/services/T12312312323123123',
     channels: {
-      exceptions: '#nodejs_exceptions'
-    }
+      exceptions: '#nodejs_exceptions',
+    },
   },
   logs: {
     dir: '/logs',
-    level: (process.env.NODE_ENV === 'production') ? 'info' : 'debug'
+    level: (process.env.NODE_ENV === 'production') ? 'info' : 'debug',
   },
 };
